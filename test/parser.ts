004aa0da4dc3056cc258d60c514f1019f0156c0d
Toru Nagashima <star.ctor@gmail.com>
2018-06-16 11:52:35 +0900

    Chore: trivial

--- test/parser.ts
+++ test/parser.ts
@@ -19,6 +19,22 @@ describe("parseRegExpLiteral function:", () => {
         const options = fixture.options
 
         describe(`${filename} (options=${JSON.stringify(options)})`, () => {
+            if (filename.indexOf("-valid") !== -1) {
+                it("should not contain any invalid test case", () => {
+                    for (const source of Object.keys(fixture.patterns)) {
+                        const result = fixture.patterns[source]
+                        assert("ast" in result, `${source} is invalid`)
+                    }
+                })
+            } else if (filename.indexOf("-invalid") !== -1) {
+                it("should not contain any valid test case", () => {
+                    for (const source of Object.keys(fixture.patterns)) {
+                        const result = fixture.patterns[source]
+                        assert("error" in result, `${source} is valid`)
+                    }
+                })
+            }
+
             for (const source of Object.keys(fixture.patterns)) {
                 const result = fixture.patterns[source]
                 if ("ast" in result) {
@@ -58,27 +74,3 @@ describe("parseRegExpLiteral function:", () => {
         assert.deepStrictEqual(actual, expected)
     })
 })
-
-for (const filename of Object.keys(Fixtures)) {
-    const fixture = Fixtures[filename]
-
-    if (filename.indexOf("-valid") !== -1) {
-        describe(filename, () => {
-            it("should not contain any invalid test case", () => {
-                for (const source of Object.keys(fixture.patterns)) {
-                    const result = fixture.patterns[source]
-                    assert("ast" in result, `${source} is invalid`)
-                }
-            })
-        })
-    } else if (filename.indexOf("-invalid") !== -1) {
-        describe(filename, () => {
-            it("should not contain any valid test case", () => {
-                for (const source of Object.keys(fixture.patterns)) {
-                    const result = fixture.patterns[source]
-                    assert("error" in result, `${source} is valid`)
-                }
-            })
-        })
-    }
-}
